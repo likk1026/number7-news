8f5ceb70d06d78a3ca6ab1d5a227c2fa72248cbb
likk1026 <62204685+likk1026@users.noreply.github.com>
2025-03-30 21:13:15 +0800

    Add files via upload

---
+++ news_onehour/db.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host='localhost',
+    user = 'root',
+    password = '1234567890',
+    database = 'allnews',
+    buffered = True
+    )
+
+
+cursor = conn.cursor()  #建立一個資料庫操作物件
